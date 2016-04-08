5cbd2688949cab4c503f4167c097ebafb7a3cdce
mahimachawla8 <mchawla8@gmail.com>
2016-04-07 20:05:23 -0400

    Initial commit

---
+++ tsb/js/app.js
@@ -0,0 +1,103 @@
+$(function () {
+
+  var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight() - 30;
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+
+    sections.each(function() {
+      var top = $(this).offset().top - nav_height,
+      bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        nav.find('a').removeClass('active');
+        sections.removeClass('active');
+
+        $(this).addClass('active');
+        nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+      }
+    });
+  });
+
+
+  nav.find('a').on('click', function () {
+    var $el = $(this)
+    , id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - nav_height
+    }, 1000);
+
+    return false;
+  });
+
+
+  $(window).scroll(function(){
+    var pos1 = $("body").scrollTop();
+    console.log(pos1);
+
+if(pos1>600) {
+  $("nav").addClass("short")
+} else {
+  $("nav").removeClass("short")
+}
+
+});
+
+
+ $("#logo").hover(
+  function(){
+  $("#tsb").addClass("fade");
+}, function(){
+  $("#tsb").removeClass("fade");
+}
+ );
+
+
+$(document).ready(function(){
+  $('.center').slick({
+    centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        string: $(".your-class"),
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        string: $(".your-class"),
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
+});
+
+$(document).ready(function(){
+ $('.faded').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
+});
+
+
+  
+
+
+});
