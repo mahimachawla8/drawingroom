3197b61a10ac51f50aeac34d5401b349bfc89f96
Mahima Chawla <mahimachawla@Mahimas-Air-2.home>
2016-05-10 22:58:38 -0400

    hidden reg 5

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
 });
 
 $("#mce-EMAIL").click(function(){
-  if $("#one").click(){
+  if ($("#one").click()){
     $("#mce-EMAIL").removeClass("mce_inline_error")
     // $(".mce_inline_error").addClass("hiddenreg")
   }
