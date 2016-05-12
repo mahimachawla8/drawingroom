18dd63f6b6e870bd30a4906a87520d4e73e6dfc3
Mahima Chawla <mahimachawla@Mahimas-MacBook-Air-2.local>
2016-05-12 14:22:54 -0400

    attempt

--- js/app.js
+++ js/app.js
@@ -91,7 +91,7 @@ $(document).ready(function(){
 $(document).click(function(){
   if($("#mce-EMAIL").hasClass("mce_inline_error")) {
     console.log("it's working");
-    $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").attr("id", "hiddenreg");
+    $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").empty();
   } else {
     console.log("it continues to work");
   }
