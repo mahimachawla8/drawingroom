19d58afe661e1fb3bda491ed58aaaa072194525c
Mahima Chawla <mahimachawla@Mahimas-MacBook-Air-2.local>
2016-05-12 17:02:39 -0400

    attemot

--- js/app.js
+++ js/app.js
@@ -91,10 +91,10 @@ $(document).ready(function(){
 $(document).click(function(){
   if($("#mce-EMAIL").hasClass("mce_inline_error")) {
     console.log("it's working");
-    $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").remove();
+    // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").remove();
     // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").empty();
     // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").html("Please enter a valid email address.");
-    // $(".mc-field-group").html()
+    $(".mc-field-group").html("<!--      <label for="mce-EMAIL">Email</label> --> <input type="email" placeholder="Email" value="" name="EMAIL" class="required email mce_inline_error" id="mce-EMAIL" aria-required="true" aria-invalid="true">");
   } else {
     console.log("it continues to work");
   }
