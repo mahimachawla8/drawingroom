c6ede3c655e9ac2b8f08fa53c6047afccc36a6a5
Mahima Chawla <mahimachawla@Mahimas-MacBook-Air-2.local>
2016-05-12 13:54:46 -0400

    attempt again

--- js/app.js
+++ js/app.js
@@ -88,6 +88,13 @@ $(document).ready(function(){
 });
 });
 
+$(document).click(function(){
+  if($("#mce-EMAIL").hasClass("mce_inline_error")) {
+    console.log("it's working");
+    $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").attr("id", "hiddenreg");
+
+  }
+})
 
 
 // $("#logo").click
