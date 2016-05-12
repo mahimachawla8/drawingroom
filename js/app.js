69d06c9ea4113c8d97a1cd70f9980eb0e992ce3a
Mahima Chawla <mahimachawla@Mahimas-MacBook-Air-2.local>
2016-05-12 17:18:48 -0400

    research

--- js/app.js
+++ js/app.js
@@ -88,17 +88,17 @@ $(document).ready(function(){
 });
 });
 
-$(document).click(function(){
-  if($("#mce-EMAIL").hasClass("mce_inline_error")) {
-    console.log("it's working");
-    // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").remove();
-    // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").empty();
-    // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").html("Please enter a valid email address.");
-    $(".mc-field-group").html("<input type="email" placeholder="Email" value="" name="EMAIL" class="required email mce_inline_error" id="mce-EMAIL" aria-required="true" aria-invalid="true">");
-  } else {
-    console.log("it continues to work");
-  }
-})
+// $(document).click(function(){
+//   if($("#mce-EMAIL").hasClass("mce_inline_error")) {
+//     console.log("it's working");
+//     // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").remove();
+//     // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").empty();
+//     // $("#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error").html("Please enter a valid email address.");
+//     // $(".mc-field-group").html("<input type="email" placeholder="Email" value="" name="EMAIL" class="required email mce_inline_error" id="mce-EMAIL" aria-required="true" aria-invalid="true">");
+//   } else {
+//     console.log("it continues to work");
+//   }
+// })
 
 
 
